fc3286968a6f2d0c06ddf8cf3ca7adb79d10a6df
Harshh-singh <hs8961524@gmail.com>
2024-06-25 18:15:51 +0530

    initial commit

---
+++ BackEnd/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/api', (req, res)=>{
+    res.json({"users":["userOne", "userTwo", "userThree"]})
+})
+
+
+app.listen(port, ()=>{
+    console.log(`App running on port ${port}`);
+})
