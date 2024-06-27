26ec90868a46945aaddb8d331bac235f7c854d0c
Harshh-singh <hs8961524@gmail.com>
2024-06-27 19:51:25 +0530

    setup env variables

--- BackEnd/config/mongoose.js
+++ BackEnd/config/mongoose.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/MovieList');
+mongoose.connect(process.env.mongodb);
 
 const db = mongoose.connection;
 db.on('error', console.log.bind(console, 'Error connecting to Mongodb'));
@@ -7,5 +9,6 @@ db.on('error', console.log.bind(console, 'Error connecting to Mongodb'));
 db.once('open', function(){
     console.log("connected to db :: Mongodb")
 });
+
 
 module.exports = db;
