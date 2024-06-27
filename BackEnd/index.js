4f7319153d4af407b15bbf27774f784bb6f93c5c
Harshh-singh <140523882+Harshh-singh@users.noreply.github.com>
2024-06-27 20:21:45 +0530

    Update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -8,7 +8,11 @@ const port = process.env.Port;
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin:https://movie-list-app-frontend-ashy.vercel.app/
+    }
+));
 
 app.use(express.json());
 app.use('/api/movies',movieRouter);
